b4a16b25b47e1fb920d668cbc27c4ee897f26cd3
MaximLaskin <maxlaskin@mail.ru>
2022-11-09 13:52:45 +0300

    Initial Commit

---
+++ Pokemon_with_Network/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokemon_with_Network
+//
+//  Created by Maxim on 09.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pokemon_with_Network/Pokemon_with_NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_with_NetworkApp.swift
+//  Pokemon_with_Network
+//
+//  Created by Maxim on 09.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_with_NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
